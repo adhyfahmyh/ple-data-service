a581d756f2357c2c03580ac9b339c38b2dd4cc8c
adhyfa <adhyfafahmy@gmail.com>
2019-04-06 17:14:10 +0700

    Update

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import save
 
 app = Flask(__name__)
 
+
 @app.route("/save_activity_log", methods=["POST"])
 def save_activity_log():
-    if request.method == "POST:
+    if request.method == "POST":
         save.save_activity_log()
+
 
 if __name__ == "__main__":
     app.run()
