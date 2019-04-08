6b00ab0d7cfb1963e846abe638d94ed02fb90bb4
adhyfa <adhyfafahmy@gmail.com>
2019-04-08 20:51:42 +0700

    update

--- save.py
+++ save.py
@@ -1,13 +1,31 @@
 import mysql.connector
 from mysql.connector import Error
 
+def is_activity_log_available(username):
+    try:
+        mySQLconnection = mysql.connector.connect(host='103.28.53.243',
+                                    database='plewebid_wp389',
+                                    user='plewebid_wp389',
+                                    password='plewebid_wp389')
+        sql_select_Query = "INSERT INTO selection (Username, ContentId, Frequency) VALUES ('"+username+"', '31', '3')"
+        cursor = mySQLconnection.cursor()
+        cursor.execute(sql_select_Query)
+    except Error as e :
+        print ("Error while connecting to MySQL", e)
+    finally:
+        #closing database connection.
+        if(mySQLconnection.is_connected()):
+            mySQLconnection.close()
+            print("MySQL connection is closed")
+
+
 def save_activity_log(username):
     try:
         mySQLconnection = mysql.connector.connect(host='103.28.53.243',
                                     database='plewebid_wp389',
                                     user='plewebid_wp389',
                                     password='plewebid_wp389')
-        sql_select_Query = "INSERT INTO selection (UserId, ContentId, Frequency) VALUES ('"+username+"', '31', '3')"
+        sql_select_Query = "INSERT INTO selection (Username, ContentId, Frequency) VALUES ('"+username+"', '31', '3')"
         cursor = mySQLconnection.cursor()
         cursor.execute(sql_select_Query)
     except Error as e :
